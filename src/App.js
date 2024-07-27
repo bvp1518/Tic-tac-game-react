37fc97a765b415e8d96a99baccddedb9e5397e36
bvp1518 <bvp18061992@gmail.com>
2024-07-27 17:41:10 +0530

    complete till video no.96

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Player from './components/Player'
+import GameBoard from './components/GameBoard';
+import Log from './components/Log';
+import { WINNING_COMBINATION } from './Winning-combination.js';
+import GameOver from './components/GameOver.js';
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+};
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard,players){
+  let winner;
+
+  for (const combination of WINNING_COMBINATION) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+
+    // console.log('firstSquareSymbol',firstSquareSymbol);
+    // console.log('secondSquareSymbol',secondSquareSymbol);
+    // console.log('gameBoard',gameBoard[combination[0].row][combination[0].col]);
+    
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+  return winner;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+  
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard,players);
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+    const currentPlayer = deriveActivePlayer(prevTurns);
+
+    const updatedTurns = [
+      { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+      ...prevTurns,
+    ];
+
+
+    return updatedTurns;
+  });
+}
+
+function handleRestart() {
+  setGameTurns([]);
+}
+
+function handlePlayerNameChange(symbol, newName) {
+  setPlayers(prevPlayers => {
+    return {
+      ...prevPlayers,
+      [symbol]: newName
+    };
+  });
+}
+
+return (
+  <main>
+    <div id="game-container">
+      <ol id="players" className='highlight-player'>
+        <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
+        <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
+      </ol>
+      {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+      <GameBoard
+        onSelectSquare={handleSelectSquare}
+        board={gameBoard} />
     </div>
-  );
+    <Log turns={gameTurns} />
+  </main>
+)
 }
 
 export default App;
